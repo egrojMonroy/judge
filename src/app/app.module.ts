25217e6a2e9157eb4ce4e266e2863a7725eab917
Jorge Monroy Barriga <egrojmonroy@gmail.com>
2018-04-03 11:53:37 -0400

    Create design sign Up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     SignInComponent,
-    NavbarComponent
+    NavbarComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule
