519ba1e0bb5dfe1bd0bf0f6829e150b63514ba1a
egrojmonroy <egrojmonroy@gmail.com>
2018-08-07 20:13:59 -0400

    Add server

--- server.js
+++ server.js
@@ -23,12 +23,12 @@ const forceSSL = function() {
 
 // Run the app by serving the static files
 // in the dist directory
-app.use(express.static(__dirname + '/dist/judge'));
+app.use(express.static(__dirname + '/dist'));
 
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(__dirname + '/dist/judge/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 // Start the app by listening on the default
